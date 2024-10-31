e2dfd551c89a9dc47b60547564d618341b9a52b1
liyin <liyincode@gmail.com>
2024-10-31 14:46:21 +0800

    feat: use hydrateRoot api to hydrate server html

--- client.js
+++ client.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+// import { createRoot } from 'react-dom/client';
+import { hydrateRoot } from "react-dom/client";
 import App from'./pages/index'
 
-const root = createRoot(document.getElementById('root'));
-root.render(<App />);
+// const root = createRoot(document.getElementById('root'));
+// root.render(<App />);
+
+hydrateRoot(document.getElementById('root'), <App />);
